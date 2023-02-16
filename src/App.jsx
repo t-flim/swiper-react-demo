daaaaad24f802f010df9097e34a10086ba05ea21
t-flim <fulim1988@gmail.com>
2023-02-16 10:59:47 +0800

    Import Carousel to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import "./App.css"
 
+// components
+import Carousel from "./components/Carousel/Carousel"
+
 function App() {
 
   return (
     <div className="app">
+      <Carousel />
     </div>
   )
 }
