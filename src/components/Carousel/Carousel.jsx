8a54f0c928f526a7eebb98a6d01b808d172deb6e
t-flim <fulim1988@gmail.com>
2023-02-17 10:17:25 +0800

    Refactor code - export slide image as module

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,6 +1,8 @@
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react"
 
+import { images as slides } from "../../info/img-data"
+
 // Import Swiper styles
 import "swiper/css"
 import "swiper/css/navigation"
@@ -12,32 +14,14 @@ import "./Carousel.css"
 import { EffectFade, Autoplay, Navigation } from "swiper"
 
 function Carousel() {
-    const slides = [
-        {
-            location: "Berlin",
-            url: "https://images.unsplash.com/photo-1560969184-10fe8719e047?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
-        },
-        {
-            location: "London",
-            url: "https://images.unsplash.com/photo-1513635269975-59663e0ac1ad?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
-        },
-        {
-            location: "Moscow",
-            url: "https://images.unsplash.com/photo-1576413326475-ea6c788332fb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80"
-        },
-        {
-            location: "Paris",
-            url: "https://images.unsplash.com/photo-1550340499-a6c60fc8287c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
-        }
-    ]
     
     return(
         <Swiper
             autoplay={{
-                delay: 3000,
+                delay: 3500,
                 disableOnInteraction: false,
             }}
-            speed={500}
+            speed={1000}
             loop={true}
             effect={"fade"}
             navigation={true}
---
+++ src/info/img-data.js
@@ -0,0 +1,42 @@
+const images = [
+    {
+        location: "Berlin",
+        url: "https://images.unsplash.com/photo-1560969184-10fe8719e047?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+    },
+    {
+        location: "London",
+        url: "https://images.unsplash.com/photo-1513635269975-59663e0ac1ad?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+    },
+    {
+        location: "Moscow",
+        url: "https://images.unsplash.com/photo-1576413326475-ea6c788332fb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80"
+    },
+    {
+        location: "Paris",
+        url: "https://images.unsplash.com/photo-1550340499-a6c60fc8287c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+    },
+    {
+        location: "Budapest",
+        url: "https://images.unsplash.com/photo-1616432902940-b7a1acbc60b3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+    },
+    {
+        location: "New York",
+        url: "https://images.unsplash.com/photo-1523374228107-6e44bd2b524e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+    },
+    {
+        location: "Kuala Lumpur",
+        url: "https://images.unsplash.com/photo-1574218732130-6b19257fd9db?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1032&q=80"
+    },
+    {
+        location: "Singapore",
+        url: "https://images.unsplash.com/photo-1496939376851-89342e90adcd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+    },
+    {
+        location: "Hong Kong",
+        url: "https://images.unsplash.com/photo-1594973782943-3314fe063f68?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
+    }
+]
+
+
+
+export { images }
